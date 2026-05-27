58a752b01d41a8aec19a6b8e540badd084e99785
uzeyireshref <uzeyireshref@users.noreply.github.com>
2026-05-28 02:13:45 +0400

    Release 1.0.17 after reverting tablet spacing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.uzeyir.photoselector"
         minSdk = 26
         targetSdk = 36
-        versionCode = 16
-        versionName = "1.0.15"
+        versionCode = 18
+        versionName = "1.0.17"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
