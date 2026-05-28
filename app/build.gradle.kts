17ce72fd8735581a837efc90a78762226eed3c1e
uzeyireshref <uzeyireshref@users.noreply.github.com>
2026-05-29 02:41:58 +0400

    Release 1.0.20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.uzeyir.photoselector"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.0.19"
+        versionCode = 21
+        versionName = "1.0.20"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
